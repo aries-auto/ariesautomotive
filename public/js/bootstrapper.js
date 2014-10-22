473bce8aa722d46cb0b0519256ba28524ede374c
Matt Mickelson <mickelmr@gmail.com>
2014-10-21 22:45:25 -0500

    moved typekit logic to bootstrapper

--- public/js/bootstrapper.js
+++ public/js/bootstrapper.js
@@ -5,6 +5,15 @@ require(['require','./config-require'],function(require, config){
 	// update global require config
 	window.require.config(config);
 
+	// load typekit fonts
+	(function(d) {
+		var config = {
+			kitId: 'gah1dnk',
+			scriptTimeout: 3000
+		},
+		h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!=="complete"&&a!=="loaded"){return}f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+	})(document);
+
 	// load app
 	require(['/js/main.js']);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -11,27 +11,6 @@ define([
 
 	// You can place operations that need to initialize prior to app start here
 	// using the `run` function on the top-level module
-	var Typekit = {};
-	var TypekitConfig = {
-		kitId: 'gah1dnk'
-	};
-
-	(function() {
-	 	var tk = document.createElement('script');
-	 	tk.src = '//use.typekit.com/' + TypekitConfig.kitId + '.js';
-	 	tk.type = 'text/javascript';
-	 	tk.async = 'true';
-	 	tk.onload = tk.onreadystatechange = function() {
-	 		var rs = this.readyState;
-	 		if (rs && rs !== 'complete' && rs !== 'loaded'){
-	 			return;
-	 		}
-	 		try { Typekit.load(TypekitConfig); } catch (e) {}
-	 	};
-
-	 	var s = document.getElementsByTagName('script')[0];
-	 	s.parentNode.insertBefore(tk, s);
-	 })();
 
 	// as script is at the very bottom of the page no waiting for domReady
 	angular.bootstrap(document, ['app']);
