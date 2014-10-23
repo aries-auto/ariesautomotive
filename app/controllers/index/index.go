096642931e06c819cbb8bd8dcc3a29c71377050f
Matt Mickelson <mickelmr@gmail.com>
2014-10-23 13:37:11 -0500

    removes martini paths, except root path

--- app/controllers/index/index.go
+++ app/controllers/index/index.go
@@ -9,39 +9,3 @@ import (
 func Index(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
 	ren.HTML(200, "homepage", bag)
 }
-
-func AboutUs(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "aboutus", bag)
-}
-
-func Category(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "category", bag)
-}
-
-func Part(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "parts", bag)
-}
-
-func ContactUs(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "contactus", bag)
-}
-
-func TechSupport(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "techsupport", bag)
-}
-
-func BecomeDealer(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "becomedealer", bag)
-}
-
-func AppGuides(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "appguides", bag)
-}
-
-func Warranties(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "warranties", bag)
-}
-
-func TermsAndConditions(rw http.ResponseWriter, req *http.Request, bag map[string]interface{}, ren render.Render) {
-	ren.HTML(200, "terms", bag)
-}
