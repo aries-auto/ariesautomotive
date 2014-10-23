4ba2525c49ad6c3f8e48e26af62385e388559870
Alex Ninneman <alex@ninneman.org>
2014-10-23 08:53:45 -0500

    fixed the ngSanitize module reference

--- public/js/controllers/category/category-ctrl.js
+++ public/js/controllers/category/category-ctrl.js
@@ -4,17 +4,19 @@
 define(['./module'], function (module) {
 	'use strict';
 
-	module.controller('CategoryController', ['$scope', '$location', 'CategoryService', 'ngSanitize' , function(scope, location, CategoryService, ngSanitize){
-		var path = location.absUrl();
+	module.controller('CategoryController', ['$scope', '$location', '$sce', 'CategoryService' , function($scope, $location, $sce, CategoryService){
+		var path = $location.absUrl();
 		var parts = path.split('/');
 		var catID = parts.length - 1 > 0 ? parseInt(parts[parts.length - 1]) : 0;
-		
+		$scope.category = {};
+
 		CategoryService.GetCategory(catID,function(cat, err){
 			if(err){
 				console.log(err);
 				return;
 			}
-			scope.category = cat;
+			$scope.category = cat;
+			console.log($sce.trustAsHtml(cat.title));
 		})
 	}]);
 });
