337076f8b56fc2a88755894cc829cc6ee615e0a3
John Shenk <john_shenk@hotmail.com>
2014-10-24 11:05:59 -0500

    techSupport - years

--- public/js/controllers/techsupport/techsupport-ctrl.js
+++ public/js/controllers/techsupport/techsupport-ctrl.js
@@ -10,21 +10,28 @@ define(['./module'], function (module) {
 		$scope.message = "Submit when ready:";
 		$scope.dateMessage;
 		
-		$scope.years = [3333,444];
+
+		var currentYear = new Date().getFullYear()+1, years = [];
+        var startYear =  1963;
+        while (currentYear >= startYear ) {
+                years.push(currentYear--);
+        } 
+        $scope.years = years;
+          
 
 
 
 		$scope.submitTechSupport = function(techSupport){
-			if(techSupport.purchaseDate == null){$scope.dateMessage = "This field cannot be empty"; return;}
+			if(techSupport.purchaseDate == null){$scope.dateMessage = "This field cannot be empty."; return;}
 			techSupport.purchaseDate = new Date(techSupport.purchaseDate);
 			
-			if(techSupport.vehicleYear != null && isNaN(techSupport.vehicleYear)){alert("Vehicle year must be a number.")};
+			if(techSupport.vehicleYear != null && isNaN(techSupport.vehicleYear)){alert("Vehicle year must be a number.");return};
 			techSupport.vehicleYear = parseInt(techSupport.vehicleYear);
 			
 			TechSupportService.SubmitTechSupport(techSupport)
 			.then(function(data){
 				$scope.techSupport = {};
-				// $scope.dateMessage = {};
+				$scope.dateMessage = {};
 				$scope.message = "Request sent.";
 			});
 		}
