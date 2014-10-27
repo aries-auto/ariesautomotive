9e655ef4c0ff4ff94563d110f02613f902876fb9
Matt Mickelson <mickelmr@gmail.com>
2014-10-26 23:16:50 -0500

    playing with contact page...leverages data-binding on contact type to display various form fields on view

--- public/js/controllers/contact/contact-ctlr.js
+++ public/js/controllers/contact/contact-ctlr.js
@@ -4,13 +4,28 @@
 define(['./module'], function (module) {
 	'use strict';
 
-	module.controller('ContactController', ['$scope', 'ContactService', 'GeographyService', function($scope, ContactService, GeographyService){
+	module.controller('ContactController', ['$scope', 'ContactService','BecomeDealerService', 'GeographyService', 
+		function($scope, ContactService, BecomeDealerService, GeographyService){
+		$scope.formData = {};
+
+		$scope.postForm = function(){
+			//todo: implement this
+		};
+
 		ContactService.GetContactTypes(function(types, err){
 			if(err){
 				console.log(err);
 				return;
 			}
 			$scope.contactTypes = types;
+		});
+
+		BecomeDealerService.GetBusinessClasses(function(classes, err){
+			if(err !== null){
+				console.log(err);
+				return;
+			}
+			$scope.businessClasses = classes;
 		});
 		
 		GeographyService.GetCountryStates(function(countries, err){
