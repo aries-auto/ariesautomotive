9cae5af88627a78ed34f03c5f4bb8eb02fc1b6eb
davidvaini <davidvaini@gmail.com>
2014-11-06 09:23:05 -0600

    added more e2e tests

--- public/js/controllers/part/index.e2e.js
+++ public/js/controllers/part/index.e2e.js
@@ -16,6 +16,19 @@ describe('E2E: Testing Part Page', function () {
 		it('should have part number listed', function(){
 			expect(element(by.css('.partNumSmall')).getText()).not.toEqual("");
 		});
+
+		it('should have 5 featured products', function(){
+			expect(element.all(by.css('.featuredProd')).count()).toBe(5);
+		});
+		
+		it('should have breadcrumbs', function(){
+			expect(element(by.css('.breadcrumb')).all(by.tagName("li")).count()).toBeGreaterThan(0);
+		});
+
+		it('should have a upc listed', function(){
+			expect(element(by.css('#upc')).getText()).not.toEqual("");
+		});
+
 	});
 
 });
