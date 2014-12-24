22535befdde9c264bc7781b4026647220f6dba4c
Matt Mickelson <mickelmr@gmail.com>
2014-12-24 12:07:46 -0600

    changes api key

--- public/js/app.js
+++ public/js/app.js
@@ -68,7 +68,7 @@ define([
 		$locationProvider.html5Mode(true);
 	}).value('AppConfig', {
 		APIURL : 'http://ariesautoapi.curtmfg.com',
-		APIKEY : 'eef1922f-2cba-11e4-8758-42010af0fd79'
+		APIKEY : '883d4046-8b96-11e4-9475-42010af00d4e'
 	}).controller('AppController', ['$scope','CategoryService', function($scope, CategoryService){
 		$scope.parentCats = [];
 		CategoryService.GetParents().then(function(parentCats){
