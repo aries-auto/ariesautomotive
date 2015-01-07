ddc6039e75c32bf88264c1fb16e8ebe3eee5936c
stinkyfingers <john_shenk@hotmail.com>
2015-01-07 09:16:28 -0600

    adds brandId to contact POST

--- public/js/controllers/becomedealer/becomedealer-ctrl.js
+++ public/js/controllers/becomedealer/becomedealer-ctrl.js
@@ -20,6 +20,7 @@ define(['./module'], function (module) {
 			contact.contactType = '15'; //becoming a dealer
 			contact.sendEmail = true;
 			contact.subject = 'Becoming a Dealer';
+			contact.brand.id = 3;
 
 			BecomeDealerService.PostContactData(contact).then(function(data){
 				$scope.contact = data;
