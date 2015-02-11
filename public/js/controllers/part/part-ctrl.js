057689240b047e35bfc8341c589d347827e1a86d
Alex Ninneman <alex@ninneman.org>
2015-02-11 13:24:09 -0600

    made some modifications/additions to part details

--- public/js/controllers/part/part-ctrl.js
+++ public/js/controllers/part/part-ctrl.js
@@ -24,5 +24,79 @@ define(['./module'], function (module) {
 		$scope.getIframeSrc = function(videoID){
 			return $sce.trustAsResourceUrl('//www.youtube.com/embed/' + videoID);
 		};
+		$scope.getCategoryBrief = function(){
+			if ($scope.part.content === undefined){
+				return '';
+			}
+			for (var i = $scope.part.content.length - 1; i >= 0; i--) {
+				var con = $scope.part.content[i];
+				if(con.contentType.Type === 'CategoryBrief'){
+					return con.text;
+				}
+			}
+			return '';
+		};
+		$scope.getHTMLDesc = function(){
+			if ($scope.part.content === undefined){
+				return '';
+			}
+			for (var i = $scope.part.content.length - 1; i >= 0; i--) {
+				var con = $scope.part.content[i];
+				if(con.contentType.Type === 'HTMLDescription'){
+					return con.text;
+				}
+			}
+			return '';
+		};
+		$scope.getBullets = function(){
+			var bulls = [];
+			if ($scope.part.content === undefined){
+				return bulls;
+			}
+			for (var i = $scope.part.content.length - 1; i >= 0; i--) {
+				var con = $scope.part.content[i];
+				if(con.contentType.Type === 'Bullet' && bulls.indexOf(con.text) === -1){
+					bulls.push(con.text);
+				}
+			}
+			return bulls;
+		};
+		$scope.getPrice = function(){
+			if($scope.part.pricing === undefined || $scope.part.pricing === null){
+				return '';
+			}
+			for (var i = $scope.part.pricing.length - 1; i >= 0; i--) {
+				var pr = $scope.part.pricing[i];
+				if(pr.type === 'Jobber'){
+					return '$'+pr.price;
+				}
+			}
+			return '';
+		};
+		$scope.getInstallSheet = function(){
+			if($scope.part === undefined || $scope.part.install_sheet === undefined){
+				return '';
+			}
+			if($scope.part === null || $scope.part.install_sheet === null){
+				return '';
+			}
+			return $scope.part.install_sheet.Scheme +'://'+$scope.part.install_sheet.Host+$scope.part.install_sheet.Path;
+		};
+		$scope.getInstallVideo = function(){
+			if($scope.part === undefined || $scope.part.videos === undefined){
+				return '';
+			}
+			if($scope.part === null || $scope.part.videos === null){
+				return '';
+			}
+
+			for (var i = $scope.part.videos.length - 1; i >= 0; i--) {
+				var vid = $scope.part.videos[i];
+				if(vid.Type === 'Installation Video'){
+					return vid.YouTubeVideoId;
+				}
+			}
+			return '';
+		};
 	}]);
 });
