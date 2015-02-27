13cfb17c629f29f143d2b71489549ec72b62fba1
Alex Ninneman <alex@ninneman.org>
2015-02-27 14:00:06 -0600

    made some form tweaks, added comnet link and removed bad pricing

--- public/js/controllers/becomedealer/becomedealer-ctrl.js
+++ public/js/controllers/becomedealer/becomedealer-ctrl.js
@@ -17,10 +17,12 @@ define(['./module'], function (module) {
 		});
 
 		$scope.saveDealer = function(contact){
-			contact.contactType = '15'; //becoming a dealer
+			contact.type = '15'; //becoming a dealer
 			contact.sendEmail = true;
 			contact.subject = 'Becoming a Dealer';
-			contact.brand.id = 3;
+			contact.brand = {
+				id: 3
+			};
 
 			BecomeDealerService.PostContactData(contact).then(function(data){
 				$scope.contact = data;
