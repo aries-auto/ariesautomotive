e470ea45d1ab1fcdc8b653ed07f5540d45226ec5
Alex Ninneman <alex@ninneman.org>
2015-06-29 13:43:01 -0500

    updated the API endpoint

--- src/app/constants/config.js
+++ src/app/constants/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('ariesautomotive').value('AppConfig', {
-	APIURL : 'http://localhost:8080',
+	APIURL : 'http://ariesautoapi.curtmfg.com',
 	APIKEY : '883d4046-8b96-11e4-9475-42010af00d4e'
 });
