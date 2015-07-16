5737b4953d600af16a69eda201400d9cb3700fd1
baseigneurie <bseig@outlook.com>
2015-07-16 09:06:10 -0500

    Testing routing options for sitemaps

--- init.go
+++ init.go
@@ -1,8 +1,9 @@
 package ariesautomotive
 
 import (
-	"github.com/unrolled/render"
 	"net/http"
+
+	"github.com/unrolled/render"
 )
 
 func init() {
@@ -12,10 +13,21 @@ func init() {
 		Extensions:    []string{".html"}, // Specify extensions to load for templates.
 		IsDevelopment: true,              // Render will now recompile the templates on every HTML response.
 	})
+
+	x := render.New(render.Options{
+		Directory:     "dist",           // Specify what path to load the templates from.
+		Extensions:    []string{".xml"}, // Specify extensions to load for templates.
+		IsDevelopment: false,            // Render will now recompile the templates on every HTML response.
+	})
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		r.HTML(w, 200, "index", nil)
+	})
+
+	mux.HandleFunc("/sitemap.xml", func(w http.ResponseWriter, req *http.Request) {
+		x.HTML(w, 200, "sitemap", nil)
 	})
 
 	// Handle all requests using net/http
