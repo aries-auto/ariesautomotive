13d171f93e708bd254e33768af506156c2c2ab03
Alex Ninneman <alex@ninneman.org>
2015-09-09 11:37:21 -0500

    disabled contact form submission while its being submitted...stops duplicates

--- src/app/controllers/becomedealer/becomedealer.js
+++ src/app/controllers/becomedealer/becomedealer.js
@@ -1,22 +1,23 @@
 'use strict';
 
-angular.module('ariesautomotive').controller('BecomeDealerController', ['$scope','BecomeDealerService','GeographyService', '$rootScope', function($scope, BecomeDealerService, GeographyService, $rootScope){
+angular.module('ariesautomotive').controller('BecomeDealerController', ['$scope', 'BecomeDealerService', 'GeographyService', '$rootScope', function($scope, BecomeDealerService, GeographyService, $rootScope) {
 	$scope.contact = {};
-	$scope.message = "";
+	$scope.message = '';
 
-	$rootScope.pageTitle = "ARIES Automotive | Become a Dealer | Become a Seller";
-  $rootScope.pageDesc = "For the retail store, distribution center and web retailer, ARIES has the marketing resources, technical support and order-fill rate to help you succeed.";
-  $rootScope.pageKywds = "aries, dealer, seller, automotive";
+	$rootScope.pageTitle = 'ARIES Automotive | Become a Dealer | Become a Seller';
+	$rootScope.pageDesc = 'For the retail store, distribution center and web retailer, ARIES has the marketing resources, technical support and order-fill rate to help you succeed.';
+	$rootScope.pageKywds = 'aries, dealer, seller, automotive';
 
-	BecomeDealerService.GetBusinessClasses().then(function(classes){
+	BecomeDealerService.GetBusinessClasses().then(function(classes) {
 		$scope.businessClasses = classes;
 	});
 
-	GeographyService.GetCountryStates().then(function(countries){
+	GeographyService.GetCountryStates().then(function(countries) {
 		$scope.countries = countries;
 	});
 
-	$scope.saveDealer = function(contact){
+	$scope.saveDealer = function(contact) {
+		$scope.saving = true;
 		contact.type = '15'; //becoming a dealer
 		contact.sendEmail = true;
 		contact.subject = 'Becoming a Dealer';
@@ -24,9 +25,13 @@ angular.module('ariesautomotive').controller('BecomeDealerController', ['$scope'
 			id: 3
 		};
 
-		BecomeDealerService.PostContactData(contact).then(function(data){
-			$scope.contact = data;
+		BecomeDealerService.PostContactData(contact).then(function(data) {
 			$scope.message = 'Request sent.';
+			$scope.contact = {};
+			$scope.saving = false;
+		}, function() {
+			$scope.message = 'Request failed to send.';
+			$scope.saving = false;
 		});
 	};
 }]);
