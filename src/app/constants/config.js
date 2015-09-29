d6493f5c2d797a737c9ee5c71f92224773fc4de2
stinkyfingers <john_shenk@hotmail.com>
2015-09-28 23:08:42 -0500

    Back to goapi, probably for real

--- src/app/constants/config.js
+++ src/app/constants/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ariesautomotive').value('AppConfig', {
-	APIURL : 'http://ariesautoapi.curtmfg.com',
+	APIURL : 'http://goapi.curtmfg.com',
 	APIKEY : '883d4046-8b96-11e4-9475-42010af00d4e',
 	BRAND_ID : '3'
 });
