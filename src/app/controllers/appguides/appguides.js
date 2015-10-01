1977fad1c7c0499fdb012c4f537b345d4d9ccb43
davidvaini <davidvaini@gmail.com>
2015-10-01 13:20:47 -0500

    updated app guides to have hardcoded links as requested

--- src/app/controllers/appguides/appguides.js
+++ src/app/controllers/appguides/appguides.js
@@ -5,6 +5,83 @@ angular.module('ariesautomotive').controller('AppGuidesController', ['$scope', '
 
 	LookupService.collections().then(function(data){
 		$scope.collections = data;
+		console.log(data);
 	});
+
+	$scope.GetFileLocation = function(col, type){
+		var pdf = "";
+		var xlsx = "";
+		switch(col) {
+			case "3 in round side bars":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%203%20IN%20SIDE%20BARS%20App%20Guide.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%203%20IN%20SIDE%20BARS%20App%20Guide.xlsx";
+				}
+			case "3 in round side bars, pro series":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%203%20IN%20PRO%20SERIES%20SIDE%20BARS%20App%20Guide.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%203%20IN%20PRO%20SERIES%20SIDE%20BARS%20App%20Guide.xlsx";
+				}
+			case "4 in oval side bars":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%204%20IN%20OVAL%20SIDE%20BARS%20App%20Guide.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%204%20IN%20OVAL%20SIDE%20BARS%20App%20Guide.xlsx";
+				}
+			case "4 in oval side bars, wheel to wheel":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%204%20IN%20OVAL%20SIDE%20BARS%20-%20W2W%20App%20Guide.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%204%20IN%20OVAL%20SIDE%20BARS%20-%20W2W%20App%20Guide.xlsx";
+				}
+			case "6 in oval side bars and mounting brackets":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%206%20IN%20OVAL%20SIDE%20BARS_MOUNTING%20BRACKETS%20App%20Guide.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%206%20IN%20OVAL%20SIDE%20BARS_MOUNTING%20BRACKETS%20App%20Guide.xlsx";
+				}
+			case "bull bars":
+				if (type == "pdf"){
+					return "http://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Merged%20Bull%20Bars.pdf";
+				}else{
+					return "http://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Merged%20Bull%20Bars.xlsx";
+				}
+			case "floor liners":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Interiors_App_Guide.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Interiors_App_Guide.xlsx";
+				}
+			case "grille guards":
+				if (type == "pdf"){
+					return "http://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Merged%Grille%20Guards.pdf";
+				}else{
+					return "http://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Merged%20Grille%20Guards.xlsx";
+				}
+			case "jeep accessories":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Modular%20Bumper%20Data%20Tables.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Modular%20Bumper%20Data%20Tables.xlsx";
+				}
+			case "jeep bumper kits and replacement parts":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Modular%20Bumper%20Data%20Tables.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/Modular%20Bumper%20Data%20Tables.xlsx";
+				}
+			case "4 in round side bars, big step":
+				if (type == "pdf"){
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%204%20IN%20BIG%20STEP%20SIDE%20BARS%20App%20Guide.pdf";
+				}else{
+					return "https://www.curtmfg.com/masterlibrary/01resources/appguides/ARIES/ARIES%204%20IN%20BIG%20STEP%20SIDE%20BARS%20App%20Guide.xlsx";
+				}
+			default:
+				return "";
+		}
+	};
+
 
 }]);
