37565a4bab1974c5fcb0e47a5351b71b07311967
davidvaini <davidvaini@gmail.com>
2015-10-02 13:43:55 -0500

    CONVERTED HERO TEXT TO TITLE CASE AS REQUESTED

--- src/app/controllers/app/app.js
+++ src/app/controllers/app/app.js
@@ -13,31 +13,31 @@ angular.module('ariesautomotive').controller('AppController', ['$scope', '$rootS
 
 	$scope.carousel_images = [{
 		image: 'http://storage.googleapis.com/aries-website/hero-images/jeep.png',
-		text: 'NEVER FEAR THE UNCERTAIN ROAD',
+		text: 'Never Fear the Uncertain Road',
 		button_text: 'VIEW BULL BARS',
 		link: '/category/332',
 		order: 5
 	}, {
 		image: 'https://storage.googleapis.com/aries-website/hero-images/GrandCherokee.png',
-		text: 'FIND OUT WHAT IT MEANS TO BE A PRO',
+		text: 'Find Out What It Means to Be a Pro',
 		button_text: 'VIEW PRO SERIES',
 		link: '/category/331',
 		order: 2
 	}, {
 		image: 'https://storage.googleapis.com/aries-website/hero-images/JeepWrangler2015.png',
-		text: 'CHOOSE YOUR CONFIGURATION AND START CUSTOMIZING',
-		button_text: 'VIEW BUMPERS',
+		text: 'Choose Your Configuration and Start Customizing',
+		button_text: 'VIEW MODULAR BUMPERS',
 		link: '/category/324',
 		order: 3
 	}, {
 		image: 'https://storage.googleapis.com/aries-website/hero-images/Floor%20Liner%20-%20Grey%20(1).jpg',
-		text: 'ARIES UNVEILS STYLEGUARD™ AS NEW NAME FOR FLOOR LINERS',
+		text: 'ARIES Unveils StyleGuard™ as New Name for Floor Liners',
 		button_text: 'READ MORE',
 		link: '/news/47',
 		order: 1
 	}, {
 		image: 'https://storage.googleapis.com/aries-website/hero-images/navyjeep.jpg',
-		text: 'DECKED OUT JEEP TO BE DONATED TO NAVY SEAL FOUNDATION',
+		text: 'Decked Out Jeep to Be Donated to Navy SEAL Foundation',
 		button_text: 'READ MORE',
 		link: '/news/48',
 		order: 4
