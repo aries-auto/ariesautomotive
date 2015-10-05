3a7916b55ab7622dcbf4ac54235acbdff36aba9f
stinkyfingers <john_shenk@hotmail.com>
2015-10-05 10:28:06 -0500

    fixes search state location

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -3,7 +3,6 @@
 angular.module('ariesautomotive').controller('NavbarController', ['$scope', '$location', '$state', function ($scope, $location, $state) {
 	$scope.search_term = '';
 	$scope.search = function(){
-		$location.path('/search/'+$scope.search_term);
-		$state.reload();
+		$state.go('search',{'term': $scope.search_term});
 	};
 }]);
