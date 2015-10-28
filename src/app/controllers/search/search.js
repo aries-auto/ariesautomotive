436e18f0fd11f52edbaf7e14c5432cee0dc20c7b
baseigneurie <bseig@outlook.com>
2015-10-28 14:56:12 -0500

    Cleaned up last commit

--- src/app/controllers/search/search.js
+++ src/app/controllers/search/search.js
@@ -44,7 +44,6 @@ angular.module('ariesautomotive').controller('SearchController', ['$scope', 'Sea
 		}
 
 		$('.pagination').css('opacity','0.6');
-		//$scope.loadingMore = true;
 
 		page++;
 		SearchService.Search($scope.query, page, count).then(function(data){
@@ -60,6 +59,7 @@ angular.module('ariesautomotive').controller('SearchController', ['$scope', 'Sea
 		});
 
 	};
+
 	$scope.scrollTo = function(elementId){
 		$location.hash(elementId);
 		$anchorScroll();
