93fa6d344798923ffd13298cc5f4266c92495e90
Alex Ninneman <alex@ninneman.org>
2016-02-08 12:00:38 -0600

    re-organized the catalogs and closed #102

--- src/app/controllers/main/main.js
+++ src/app/controllers/main/main.js
@@ -3,6 +3,23 @@
 angular.module('ariesautomotive').controller('MainController', ['$scope', 'TestimonialService', 'PartService', '$rootScope', 'Lightbox', '$sce', function($scope, TestimonialService, PartService, $rootScope, Lightbox, $sce){
   $scope.testimonials = [];
   $scope.featuredProducts = [];
+  $scope.catalogs = [
+      {
+          title: '2016 Exterior Catalog',
+          image: 'https://storage.googleapis.com/aries-website/ARIES-Exterior-Cover.png',
+          link: 'https://storage.googleapis.com/aries-website/2016-Exterior-Catalog.pdf',
+      },
+      {
+          title: '2016 Interior Catalog',
+          image: 'https://storage.googleapis.com/aries-website/ARIES-Interior-Cover.png',
+          link: 'https://storage.googleapis.com/aries-website/2016-Interior-Catalog.pdf',
+      },
+      {
+          title: '2016 Jeep Catalog',
+          image: 'https://storage.googleapis.com/aries-website/ARIES-Jeep-Cover.png',
+          link: 'https://storage.googleapis.com/aries-website/_2016%20ARIES%20Jeep%20Catalog-SFS.pdf',
+      }
+  ];
 
   $rootScope.pageTitle = "ARIES Automotive | Custom Truck, Jeep, and SUV Accessories | Main";
   $rootScope.pageDesc = "From grille guards and modular Jeep bumpers to side bars, bull bars and floor liners, ARIES truck and SUV accessories offer a custom fit for your vehicle.";
