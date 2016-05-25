872d9a8c06dc9bb1db569823a0f91d43febad615
stinkyfingers <john_shenk@hotmail.com>
2016-05-25 11:13:47 -0500

    envision protocol issue

--- src/components/Envision/Envision.js
+++ src/components/Envision/Envision.js
@@ -10,6 +10,7 @@ class Envision extends Component {
 
 	static propTypes = {
 		className: PropTypes.string,
+		protocol: PropTypes.string,
 	};
 
 	static contextTypes = {
@@ -41,7 +42,7 @@ class Envision extends Component {
 							<h3>Envision ARIES</h3>
 							<p>See what your vehicle will look like with ARIES accessories fully installed, even before deciding which ones you actually want! Simply select your year, make, model, style and color. Then select one or more accessories to preview. When you're satisfied with your selections, connect to one of our dealers by going to <a href="/buy">Where to Buy</a>.</p>
 							<div id="icf_page"></div>
-							<script src="http://ver1.iconfigurators.com/src/embed.cfm?ky=539D7C9D0B8B72F4966C"></script>
+							{this.props.protocol === 'https:' ? (<script src="https://ver1.iconfigurators.com/src/embed.cfm?ky=539D7C9D0B8B72F4966C"></script>) : (<script src="http://ver1.iconfigurators.com/src/embed.cfm?ky=539D7C9D0B8B72F4966C"></script>)};
 						</div>
 					</div>
 				</div>
