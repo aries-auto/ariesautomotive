207852710bbb2fc4f2ea31db3243ab18ffa2675a
Alex Ninneman alex@ninneman.org <alex@ninneman.org>
2016-09-13 12:42:27 -0500

    removed overflow from header

--- src/components/Envision/Envision.js
+++ src/components/Envision/Envision.js
@@ -35,7 +35,6 @@ class Envision extends Component {
 				<p>See what your vehicle will look like with ARIES accessories fully installed, even before deciding which ones you actually want! Simply select your year, make, model, style and color. Then select one or more accessories to preview. When you're satisfied with your selections, connect to one of our dealers by going to <a href="/buy">Where to Buy</a>.</p>
 				<div id="icf_page"></div>
 				<script src="http://ver1.iconfigurators.com/src/embed.cfm?ky=539D7C9D0B8B72F4966C"></script>
-				<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 			</div>
 		);
 	}
