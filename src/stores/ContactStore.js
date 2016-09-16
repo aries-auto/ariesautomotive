4bf5c8238c432a9a57231f722b9e806a0a2f772a
baseigneurie <baseigneurie@gmail.com>
2016-09-16 13:21:34 -0500

    ContactStore now sends email

--- src/stores/ContactStore.js
+++ src/stores/ContactStore.js
@@ -2,7 +2,7 @@ import AppDispatcher from '../dispatchers/AppDispatcher';
 import events from 'events';
 import ContactActions from '../actions/ContactActions';
 import ContactSource from '../sources/ContactSource';
-import { apiBase, apiKey } from '../config';
+import { apiBase, apiKey, brand } from '../config';
 
 const EventEmitter = events.EventEmitter;
 
@@ -18,6 +18,7 @@ class ContactStore extends EventEmitter {
 			handleUpdateTypes: ContactActions.UPDATE_TYPES,
 			handleFailedTypes: ContactActions.FAILED_TYPES,
 			handleSetInput: ContactActions.SET_INPUT,
+			postContactData: ContactActions.postContactData,
 			all: ContactActions.fetchTypes,
 		});
 
@@ -55,10 +56,35 @@ class ContactStore extends EventEmitter {
 			return;
 		}
 
-		this.state.inputs[input.name] = input.name;
+		this.state.inputs[input.name] = input.value;
 		this.setState({
 			inputs: this.state.inputs,
 		});
+	}
+
+	postContactData(args) {
+		const frm = this.state.inputs;
+		frm.reason = frm.reason ? frm.reason : args;
+
+		const inputs = JSON.stringify(frm);
+		try {
+			// fetch(`${apiBase}/${frm.reason}?key=${apiKey}&brand=${brand.id}`, {
+			fetch(`http://localhost:8080/contact/${frm.reason}?key=${apiKey}&brand=${brand.id}`, {
+				method: 'post',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: inputs,
+			}).then((resp) => {
+				console.log(resp);
+				this.setState({ error: null });
+				return resp.json();
+			});
+		} catch (err) {
+			this.setState({
+				error: err.message,
+			});
+		}
 	}
 
 	async all() {
