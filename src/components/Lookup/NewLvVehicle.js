5df328e1ecc8b3b8caec49ad399b6082e07b5008
baseigneurie <baseigneurie@gmail.com>
2016-10-05 10:11:25 -0500

    Adding files

---
+++ src/components/Lookup/NewLvVehicle.js
@@ -0,0 +1,184 @@
+import React, { Component, PropTypes } from 'react';
+import cx from 'classnames';
+import s from './NewVehicle.scss';
+import Select from './Select';
+import LuverneActions from '../../actions/LuverneActions';
+import LuverneStore from '../../stores/LuverneStore';
+import withStyles from '../../decorators/withStyles';
+import connectToStores from 'alt-utils/lib/connectToStores';
+
+@withStyles(s)
+@connectToStores
+class NewLvVehicle extends Component {
+
+	static propTypes = {
+		className: PropTypes.string,
+		onSubmit: PropTypes.func.isRequired,
+		vehicle: PropTypes.shape({
+			base_vehicle: PropTypes.shape({
+				year: PropTypes.string,
+				make: PropTypes.string,
+				model: PropTypes.string,
+			}),
+			available_years: PropTypes.array,
+			available_makes: PropTypes.array,
+			available_models: PropTypes.array,
+			lookup_category: PropTypes.array,
+			products: PropTypes.array,
+		}),
+	};
+
+	static defaultProps = {
+		vehicle: {
+			base_vehicle: {
+				year: '',
+				make: '',
+				model: '',
+			},
+			available_years: [],
+			available_makes: [],
+			available_models: [],
+			lookup_category: [],
+			products: [],
+		},
+	};
+
+	constructor() {
+		super();
+
+		this.changeVehicle = this.changeVehicle.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.isActive = this.isActive.bind(this);
+	}
+
+	static getStores() {
+		return [LuverneStore];
+	}
+
+	static getPropsFromStores() {
+		return LuverneStore.getState();
+	}
+
+	resetVehicle() {
+		LuverneActions.setVehicle('', '', '');
+	}
+
+	changeVehicle(event) {
+		if (event.target === undefined || event.target.name === undefined) {
+			return;
+		}
+
+		let v = {};
+		switch (event.target.name.toLowerCase()) {
+		case 'year':
+			v = {
+				year: event.target.value,
+				make: '',
+				model: '',
+			};
+			break;
+		case 'make':
+			v = {
+				year: this.props.vehicle.base_vehicle.year,
+				make: event.target.value,
+				model: '',
+			};
+			break;
+		case 'model':
+			v = {
+				year: this.props.vehicle.base_vehicle.year,
+				make: this.props.vehicle.base_vehicle.make,
+				model: event.target.value,
+			};
+			break;
+		default:
+			break;
+		}
+
+		LuverneStore.fetchVehicle(v.year, v.make, v.model);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+
+		this.props.onSubmit();
+	}
+
+	isActive(prop) {
+		const v = this.props.vehicle;
+		switch (prop) {
+		case 'year':
+			if (!v.year || v.year === '') {
+				return s.active;
+			}
+			return '';
+		case 'make':
+			if (!v.year || v.year === '') {
+				return '';
+			}
+
+			if (!v.make || v.make === '') {
+				return s.active;
+			}
+			return '';
+		case 'model':
+			if (!v.year || v.year === '') {
+				return '';
+			}
+
+			if (!v.make || v.make === '') {
+				return '';
+			}
+
+			return s.active;
+		default:
+			return '';
+		}
+	}
+
+	render() {
+		return (
+			<form className={s.root} onSubmit={this.handleSubmit}>
+				<label className={s.heading}>Vehicle Lookup</label>
+				<Select
+					className={cx(s.formGroup, this.isActive('year'))}
+					name="year"
+					change={this.changeVehicle}
+					aria="year_lookup_label"
+					placeholder="- Select Year -"
+					values={this.props.vehicle.available_years}
+				/>
+				<Select
+					className={cx(s.formGroup, this.isActive('make'))}
+					name="make"
+					change={this.changeVehicle}
+					aria="make_lookup_label"
+					placeholder="- Select Make -"
+					values={this.props.vehicle.available_makes}
+					disabled={(!this.props.vehicle.available_makes || this.props.vehicle.available_makes.length === 0)}
+					disabledClassName={s.disabled}
+				/>
+				<Select
+					className={cx(s.formGroup, this.isActive('model'))}
+					name="model"
+					change={this.changeVehicle}
+					aria="model_lookup_label"
+					placeholder="- Select Model -"
+					values={this.props.vehicle.available_models}
+					disabled={(!this.props.vehicle.available_models || this.props.vehicle.available_models.length === 0)}
+					disabledClassName={s.disabled}
+				/>
+				<button
+					className={cx('red-transparent-button', s.viewParts)}
+					type="submit"
+					disabled={(this.props.vehicle.base_vehicle.model === '')}
+				>
+					View Parts
+				</button>
+			</form>
+		);
+	}
+
+}
+
+export default NewLvVehicle;
---
+++ src/components/VehicleResults/LuverneResults.js
@@ -0,0 +1,188 @@
+import React, { Component, PropTypes } from 'react';
+import Link from '../Link';
+import s from './VehicleResults.scss';
+import CategorizedResult from './CategorizedResult';
+import withStyles from '../../decorators/withStyles';
+import LuverneStore from '../../stores/LuverneStore';
+import CategoryStore from '../../stores/CategoryStore';
+import connectToStores from 'alt-utils/lib/connectToStores';
+// import VehicleStyle from './VehicleStyle';
+import Envision from './Envision';
+import Configurator from './Configurator';
+
+@withStyles(s)
+@connectToStores
+class LuverneResults extends Component {
+
+	static propTypes = {
+		className: PropTypes.string,
+		activeIndex: PropTypes.number,
+		context: PropTypes.shape({
+			params: PropTypes.shape({
+				year: PropTypes.string,
+				make: PropTypes.string,
+				model: PropTypes.string,
+			}),
+			iconMediaVehicle: PropTypes.object,
+			iconParts: PropTypes.oneOfType([
+				React.PropTypes.object,
+				React.PropTypes.array,
+			]),
+			vehicleParts: PropTypes.array,
+		}),
+		vehicle: PropTypes.shape({
+			base: PropTypes.shape({
+				year: PropTypes.string,
+				make: PropTypes.string,
+				model: PropTypes.string,
+			}),
+			availableYears: PropTypes.array,
+			availableMakes: PropTypes.array,
+			availableModels: PropTypes.array,
+			lookup_category: PropTypes.array,
+			products: PropTypes.array,
+		}),
+		categories: PropTypes.array,
+		activeCategory: PropTypes.object,
+		fitments: PropTypes.array,
+		error: PropTypes.object,
+		iconMediaVehicle: PropTypes.object,
+		win: PropTypes.object,
+	};
+
+	static contextTypes = {
+		onSetTitle: PropTypes.func.isRequired,
+		onPageNotFound: PropTypes.func.isRequired,
+		onSetMeta: PropTypes.func.isRequired,
+		seo: PropTypes.func.isRequired,
+	};
+
+	constructor() {
+		super();
+		this.state = {
+			context: {},
+			activeKey: '0',
+			activeCat: 0,
+		};
+		this.getMatched = this.getMatched.bind(this);
+		this.createParentItem = this.createParentItem.bind(this);
+	}
+
+	componentWillMount() {
+		const base = this.props.context ? this.props.context.params : {};
+		let title = 'Vehicle Results';
+		if (base.year && base.make && base.model) {
+			title = `${base.year} ${base.make} ${base.model} Fitment Results`;
+		}
+		this.context.onSetTitle(title);
+		this.context.onSetMeta('description', title);
+		this.context.seo({
+			title,
+			description: 'ARIES Automotive parts for ' + title,
+		});
+	}
+
+	static getStores() {
+		return [LuverneStore, CategoryStore];
+	}
+
+	static getPropsFromStores() {
+		return {
+			...LuverneStore.getState(),
+			...CategoryStore.getState(),
+		};
+	}
+
+	getMatched() {
+		if (
+			!this.props.vehicle ||
+			!this.props.vehicle.lookup_category ||
+			this.props.vehicle.lookup_category.length === 0 ||
+			!this.props.categories ||
+			this.props.categories.length === 0
+		) {
+			return <span></span>;
+		}
+
+		const groups = [];
+		const categoriesGroup = {
+			children: [],
+		};
+
+		if (this.props.categories && this.props.categories.length > 0) {
+			this.props.categories.sort((a, b) => a.sort > b.sort);
+		}
+		this.props.categories.map((cat) => {
+			const tmp = this.createParentItem(cat);
+			categoriesGroup.children = categoriesGroup.children.concat(tmp.children);
+		});
+		categoriesGroup.children.map((c) => {
+			if (!c.children || !c.children.length === 0) {
+				return;
+			}
+
+			let subs = [];
+			(c.children || []).map((cat) => {
+				const tmp = this.props.vehicle.lookup_category.filter((t) => t.category.id === cat.cat.id);
+				if (tmp.length > 0) {
+					subs = subs.concat(tmp);
+				}
+			});
+			if (subs.length > 0) {
+				groups.push(
+					<CategorizedResult
+						activeIndex={this.props.activeIndex}
+						parent={c}
+						subs={subs}
+						fitments={this.props.fitments}
+						key={groups.length}
+						iconParts={this.props.context.iconParts}
+					/>
+				);
+			}
+		});
+
+		return groups;
+	}
+
+	createParentItem(cat) {
+		if (cat.children && cat.children.length > 0) {
+			cat.children.sort((a, b) => a.sort > b.sort);
+		}
+		const newCat = {
+			cat,
+			title: cat.title,
+			children: cat.children.map(this.createParentItem),
+		};
+
+		return newCat;
+	}
+
+	render() {
+		const matched = this.getMatched();
+		return (
+			<div className={s.root}>
+				<ol className="breadcrumb">
+					<li><Link to="/" title="Home">Home</Link></li>
+					<li className="active">Vehicle Look Up Results</li>
+				</ol>
+				<div>
+					<h1>VEHICLE LOOK UP RESULTS</h1>
+					<p>Choose a category below to see the products that fit your vehicle.
+						Some products may require the style of the vehicle to be specified.
+					</p>
+				</div>
+				<div className={s.visual}>
+					{this.props.context.vehicleParts ? <Configurator win={this.props.win} className={s.configurator} context={this.props.context} /> : null}
+					{this.props.context.vehicleParts ? <Envision className={s.envision} /> : null}
+				</div>
+				<div className={s.matched}>
+					{matched}
+				</div>
+			</div>
+		);
+	}
+
+}
+
+export default LuverneResults;
