dc25fa459605a24d1de48327816b81eb69cc6754
davidvaini <davidvaini@gmail.com>
2016-10-21 15:26:04 -0500

    got vehicle cache to work properly, envision broken?

--- src/stores/VehicleStore.js
+++ src/stores/VehicleStore.js
@@ -81,6 +81,11 @@ class VehicleStore extends EventEmitter {
 		if (v.base.model !== '' && !v.availableModels) {
 			v.availableModels = this.state.vehicle.availableModels;
 		}
+
+		if (v.base.year === '' || v.base.make === '' || v.base.model === '') {
+			v.products = this.state.vehicle.products;
+			v.lookup_category = this.state.vehicle.lookup_category;
+		}
 		this.setState({
 			vehicle: v,
 			error: null,
@@ -98,6 +103,9 @@ class VehicleStore extends EventEmitter {
 	}
 
 	handleUpdateEnvision(data) {
+		if (!data.resp) {
+			return null;
+		}
 		this.setState({
 			envision: {
 				vehicleParts: this.state.envision.matchedProducts.map((p) => p.part_number),
