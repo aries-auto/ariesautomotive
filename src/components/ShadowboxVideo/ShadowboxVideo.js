89011e2dd9117ebed5d5869da7a67daba8122444
davidvaini <davidvaini@gmail.com>
2016-12-14 13:05:49 -0600

    implemented fix for rel=0 for videos, fixes #247

--- src/components/ShadowboxVideo/ShadowboxVideo.js
+++ src/components/ShadowboxVideo/ShadowboxVideo.js
@@ -19,12 +19,13 @@ class ShadowboxVideo extends Component {
 		if (!this.props.video) {
 			return null;
 		}
-
+		// we build the embed code to control rel until its fixed in the admin and in all existing videos.
+		const tmpEmbedCode = `<iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/${this.props.video.channel[0].foreign_id}?rel=0\" frameborder=\"0\" allowfullscreen></iframe>`;
 		return (
 			<div className={s.root} onClick={this.close}>
 				<div
 					className={s.videoModal}
-					dangerouslySetInnerHTML={{ __html: this.props.video.channel[0].embed_code }}
+					dangerouslySetInnerHTML={{ __html: tmpEmbedCode }}
 				></div>
 			</div>
 		);
