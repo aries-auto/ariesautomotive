6797c3ee14aad80e933257ada87941449547773f
Lehman Black <lblack@curtmfg.com>
2016-12-15 12:56:18 -0600

    Fix #250 Dealer Locator not working Shuffle logic did not take in to account cases where a tier (Gold, Silver, Platinum) was missing from the dealers.

--- src/components/WhereToBuy/Locations/Locations.js
+++ src/components/WhereToBuy/Locations/Locations.js
@@ -53,6 +53,9 @@ class Locations extends Component {
 	}
 
 	shuffle(array) {
+		if (!(array && array.length)) {
+			return [];
+		}
 		for (let i = array.length - 1; i > 0; i--) {
 			const j = Math.floor(Math.random() * (i + 1));
 			const temp = array[i];
