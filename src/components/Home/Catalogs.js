e8b604c144367d899a025284a97171c1710388a4
Alex Ninneman <alex@ninneman.org>
2016-12-20 11:46:18 -0600

    Removed `Exterior` from catalog title

--- src/components/Home/Catalogs.js
+++ src/components/Home/Catalogs.js
@@ -6,7 +6,7 @@ import Catalog from './Catalog';
 
 const catalogs = [
 	{
-		title: '2017 Exterior Catalog',
+		title: '2017 Catalog',
 		image: 'https://storage.googleapis.com/aries_catalog/ARIES_2017_Cover.jpg',
 		link: 'https://storage.googleapis.com/aries_catalog/ARIES%202017%20Catalog.pdf',
 	},
