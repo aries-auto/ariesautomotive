d7f506a7ada0ba27a2eed58ab7a2a106d5f25b34
davidvaini <davidvaini@gmail.com>
2017-04-14 13:49:38 -0500

    moved feedback button to be a link in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import s from './Footer.scss';
 import cx from 'classnames';
 import withStyles from '../../decorators/withStyles';
-import { Glyphicon } from 'react-bootstrap';
 import { brand } from '../../config';
 import footer from '../../data/footer';
 import SiteStore from '../../stores/SiteStore';
@@ -55,79 +54,74 @@ class Footer extends Component {
 			background: "url('https://storage.googleapis.com/luverne/website/misc-images/footerBackground.png')",
 		};
 		return (
-			<div>
-				<div className={s.feedbackcontainer}>
-					<div id="jirafeedback" className={s.feedbackbtn}>
-						<div className={s.feedbackGlyphContainer}><Glyphicon glyph="pencil" /></div>
-						<div className={s.feedbackTextContainer}><span>PROVIDE FEEDBACK</span></div>
-					</div>
-				</div>
-				<footer>
-					<div className={cx(s.root, 'row', 'footer-outer-row')} style={styles}>
-						<div className={cx('container', s.container)}>
-							<div className={cx('col-xs-12', 'col-sm-3', 'col-md-3', 'col-lg-3')}>
-								<h4>REACH US</h4>
-								<address itemScope itemType="//schema.org/Organization">
-									<div className={s.phoneNumber}>
-										<img src="https://storage.googleapis.com/aries-website/site-assets/phone.png" alt="phone image" />
-										<span className={s.phone}>
-											CALL
-											<a itemProp="telephone" href="tel:+18888002743" title="Call Us (888) 800-2743">
-												(888) 800-2743
-											</a>
-										</span>
-									</div>
-									<span itemProp="name" className={s.addressName}>{brand.name}</span>
-									<div className={s.addressInfo} itemProp="address" itemScope itemType="//schema.org/PostalAddress">
-										<span itemProp="streetAddress">6208 Industrial Drive</span>
-										<br />
-										<span itemProp="addressLocality">Eau Claire,</span>
-										<span itemProp="addressRegion">WI</span>
-										<span itemProp="postalCode">54701</span>
-									</div>
-								</address>
-							</div>
-							<div className={cx('col-xs-6', 'col-sm-3', 'col-md-2', 'col-lg-2', s.footerNav)}>
-								{this.renderFooterNav()}
-							</div>
-							<div className="col-xs-6 col-sm-3 col-md-4 col-lg-3">
-								<div className={cx('row', 'col-lg-10', 'col-md-10', 'col-xs-8', s.social)}>
-									<h4 className="hidden-xs hidden-sm">GET THE LATEST</h4>
-									<div className="col-xs-12 col-md-12 col-lg-12">
-										<a href={brand.facebook.link} title="Visit us on Facebook">
-											<img src="https://storage.googleapis.com/aries-website/site-assets/facebook-icon.png" alt="Facebook Logo" />
+			<footer>
+				<div className={cx(s.root, 'row', 'footer-outer-row')} style={styles}>
+					<div className={cx('container', s.container)}>
+						<div className={cx('col-xs-12', 'col-sm-3', 'col-md-3', 'col-lg-3')}>
+							<h4>REACH US</h4>
+							<address itemScope itemType="//schema.org/Organization">
+								<div className={s.phoneNumber}>
+									<img src="https://storage.googleapis.com/aries-website/site-assets/phone.png" alt="phone image" />
+									<span className={s.phone}>
+										CALL
+										<a itemProp="telephone" href="tel:+18888002743" title="Call Us (888) 800-2743">
+											(888) 800-2743
 										</a>
-										<a href={`https://twitter.com/${brand.twitter}`} title="Visit us on Twitter">
-											<img src="https://storage.googleapis.com/aries-website/site-assets/twitter-icon.png" alt="Twitter Logo" />
-										</a>
-										<a href={`http://www.youtube.com/user/${brand.youtube}`} title="Visit us on YouTube">
-											<img src="https://storage.googleapis.com/aries-website/site-assets/youtube-icon.png" alt="YouTube Logo" />
-										</a>
-									</div>
+									</span>
 								</div>
-								<div className="row col-lg-2 col-md-2 col-xs-4">
-									<div className={s.semaLogo}>
-										<a href="http://www.sema.org" title="SEMA Website">
-											<img src="https://storage.googleapis.com/aries-website/site-assets/sema-logo.png" width="64px" alt="SEMA Logo" />
-										</a>
-									</div>
+								<span itemProp="name" className={s.addressName}>{brand.name}</span>
+								<div className={s.addressInfo} itemProp="address" itemScope itemType="//schema.org/PostalAddress">
+									<span itemProp="streetAddress">6208 Industrial Drive</span>
+									<br />
+									<span itemProp="addressLocality">Eau Claire,</span>
+									<span itemProp="addressRegion">WI</span>
+									<span itemProp="postalCode">54701</span>
+								</div>
+							</address>
+						</div>
+						<div className={cx('col-xs-6', 'col-sm-3', 'col-md-2', 'col-lg-2', s.footerNav)}>
+							{this.renderFooterNav()}
+						</div>
+						<div className="col-xs-6 col-sm-3 col-md-4 col-lg-3">
+							<div className={cx('row', 'col-lg-10', 'col-md-10', 'col-xs-8', s.social)}>
+								<h4 className="hidden-xs hidden-sm">GET THE LATEST</h4>
+								<div className="col-xs-12 col-md-12 col-lg-12">
+									<a href={brand.facebook.link} title="Visit us on Facebook">
+										<img src="https://storage.googleapis.com/aries-website/site-assets/facebook-icon.png" alt="Facebook Logo" />
+									</a>
+									<a href={`https://twitter.com/${brand.twitter}`} title="Visit us on Twitter">
+										<img src="https://storage.googleapis.com/aries-website/site-assets/twitter-icon.png" alt="Twitter Logo" />
+									</a>
+									<a href={`http://www.youtube.com/user/${brand.youtube}`} title="Visit us on YouTube">
+										<img src="https://storage.googleapis.com/aries-website/site-assets/youtube-icon.png" alt="YouTube Logo" />
+									</a>
 								</div>
 							</div>
-							<div className="col-xs-12 col-sm-3 col-md-3 col-lg-4">
-								<img src={brand.footerLogo} alt={brand.name} className={cx('img-responsive', s.footerLogo)} />
-								<div className="clearfix">&nbsp;</div>
-								<br />
-								<span className={s.conditions}>
-									&copy;
-									{brand.copyrightStart ? `${brand.copyrightStart} - ${this.getYear()}` : this.getYear()}
-									{brand.name}
-									<a href="/terms" className={s.terms}>TERMS & CONDITIONS</a>
-								</span>
+							<div className="row col-lg-2 col-md-2 col-xs-4">
+								<div className={s.semaLogo}>
+									<a href="http://www.sema.org" title="SEMA Website">
+										<img src="https://storage.googleapis.com/aries-website/site-assets/sema-logo.png" width="64px" alt="SEMA Logo" />
+									</a>
+								</div>
 							</div>
 						</div>
+						<div className="col-xs-12 col-sm-3 col-md-3 col-lg-4">
+							<img src={brand.footerLogo} alt={brand.name} className={cx('img-responsive', s.footerLogo)} />
+							<div className="clearfix">&nbsp;</div>
+							<br />
+							<span className={s.conditions}>
+								&copy;
+								{brand.copyrightStart ? `${brand.copyrightStart} - ${this.getYear()}` : this.getYear()}
+								{brand.name}
+								<a href="/terms" className={s.terms}>TERMS & CONDITIONS</a>
+							</span>
+							<br />
+							<br />
+							<span className={s.jiraFeedbackContainer}><a href="#" id="jirafeedback">PROVIDE FEEDBACK</a></span>
+						</div>
 					</div>
-				</footer>
-			</div>
+				</div>
+			</footer>
 		);
 	}
 
